42e6a168a87f675867de9e13a15a3c85dcf9b545
unknown <kvrah.eo.2013@gmail.com>
2023-10-03 17:36:37 -0300

    versao navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Home from './paginas/home/Home.css';
 import './App.css';
-
+import Home from './paginas/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+);
 }
-
 export default App;
