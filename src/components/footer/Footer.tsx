4963c23e439661705266c6c1f41dd1b572d4afe1
unknown <kvrah.eo.2013@gmail.com>
2023-10-16 12:19:39 -0300

    versão final para deploy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,6 +11,10 @@ function Footer() {
   let data = new Date().getFullYear()
 
   if(usuario.token !== '') {
+    const linkedinLink = 'https://www.linkedin.com/in/richard-rcampos/'; 
+    const instagramLink = 'https://www.instagram.com/kvra.h/'; 
+    const facebookLink = 'https://www.facebook.com/kvrah'; 
+  
     footerComponent = (
       <>
         <div className="flex justify-center bg-indigo-900 text-white">
@@ -18,14 +22,20 @@ function Footer() {
             <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
+              <a href={linkedinLink} target="_blank" rel="noopener noreferrer">
+                <LinkedinLogo size={48} weight='bold' />
+              </a>
+              <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                <InstagramLogo size={48} weight='bold' />
+              </a>
+              <a href={facebookLink} target="_blank" rel="noopener noreferrer">
+                <FacebookLogo size={48} weight='bold' />
+              </a>
             </div>
           </div>
         </div>
       </>
-    )
+    );
   }
 
   return (
