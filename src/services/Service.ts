fb6ed87bdcb554f3014ed6e954bea1aa286a9f7a
unknown <kvrah.eo.2013@gmail.com>
2023-10-17 22:30:10 -0300

    teste

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'https://blogpessoalrich.onrender.com/'
-})
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados)
